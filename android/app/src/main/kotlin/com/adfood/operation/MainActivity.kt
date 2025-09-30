6ba0c5ad913d2e5ceede8e6a89e5fb9ca4b5c1bb
Perdi <perdi@fungsitama.com>
2025-09-30 11:45:03 +0700

    change

--- android/app/src/main/kotlin/com/adfood/operation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.adfood.operation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/fungsitama/hris/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fungsitama.hris
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
